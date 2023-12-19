23867a2defc57a1a40db79af9521c8f7530842a2
devxavierr <devxavierr@gmail.com>
2023-12-19 14:07:50 -0300

    implement listagem paginada de Product

--- src/main/java/com/xavier/dscommerce/services/ProductService.java
+++ src/main/java/com/xavier/dscommerce/services/ProductService.java
@@ -4,10 +4,12 @@ import com.xavier.dscommerce.Repositories.ProductRepository;
 import com.xavier.dscommerce.dto.ProductDTO;
 import com.xavier.dscommerce.entities.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
+import java.util.List;
 
 @Service
 public class ProductService {
@@ -17,6 +19,12 @@ public class ProductService {
     public ProductDTO findById(Long id){
         Product product = productRepository.findById(id).get();
         return new ProductDTO(product);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable){
+        Page<Product> product = productRepository.findAll(pageable);
+        return product.map(x-> new ProductDTO(x));
     }
 
 }
