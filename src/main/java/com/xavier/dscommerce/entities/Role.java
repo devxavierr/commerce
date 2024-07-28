b5a78a484f8f79e9dc7e78fecb80e7ae77a4f4ee
devxavierr <devxavierr@gmail.com>
2024-07-28 16:40:02 -0300

    UserDetails, GrantedAuthority, not tested

--- src/main/java/com/xavier/dscommerce/entities/Role.java
+++ src/main/java/com/xavier/dscommerce/entities/Role.java
@@ -1,12 +1,13 @@
 package com.xavier.dscommerce.entities;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
 
 import java.util.Objects;
 
 @Entity
 @Table(name = "tb_role")
-public class Role {
+public class Role implements GrantedAuthority {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -28,7 +29,8 @@ public class Role {
         this.id = id;
     }
 
-    public String getAuthority() {
+   @Override
+   public String getAuthority() {
         return authority;
     }
 
--- src/main/java/com/xavier/dscommerce/entities/User.java
+++ src/main/java/com/xavier/dscommerce/entities/User.java
@@ -1,13 +1,18 @@
 package com.xavier.dscommerce.entities;
 
 import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.*;
 
 @Entity
 @Table(name = "tb_user")
-public class User {
+public class User implements UserDetails, Serializable {
+    private static final long serialVersionUID = 1L;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -19,13 +24,13 @@ public class User {
     private String password;
 
     @OneToMany(mappedBy = "client")
-    private List<Order> orders = new ArrayList<>();
+    private transient List<Order> orders = new ArrayList<>();
 
     @ManyToMany
     @JoinTable(name = "tb_user_role",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
-    private Set<Role> roles = new HashSet<>();
+    private transient Set<Role> roles = new HashSet<>();
 
     public User(){
     }
@@ -121,5 +126,35 @@ public class User {
     @Override
     public int hashCode() {
         return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
