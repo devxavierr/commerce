519379c5d501b2e35afb836f4ea8340007120f80
devxavierr <devxavierr@gmail.com>
2024-07-28 15:24:38 -0300

    otimizando consulta SQL

--- src/main/java/com/xavier/dscommerce/Repositories/ProductRepository.java
+++ src/main/java/com/xavier/dscommerce/Repositories/ProductRepository.java
@@ -1,9 +1,17 @@
 package com.xavier.dscommerce.Repositories;
 
 import com.xavier.dscommerce.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query("select obj " +
+            "from Product obj " +
+            "where upper(obj.name) like upper(concat('%', :name, '%'))")
+    Page<Product> searchByName(String name, Pageable pageable);
 }
